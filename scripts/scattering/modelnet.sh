435cb87bf58039561c0e11237ce7a0c5512aaa95
edebrouwer <edward.debrouwer@gmail.com>
2023-07-04 13:25:56 -0400

    script

--- scripts/scattering/modelnet.sh
+++ scripts/scattering/modelnet.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --job-name=scattering_modelnet
 #SBATCH --time=20:00:00
-#SBATCH --cpus-per-task=20
+#SBATCH --cpus-per-task=12
 #SBATCH --mem=64G
 #SBATCH --output=./logs/slurm/%x_%j.out
 #SBATCH --error=./logs/slurm/%x_%j.err
