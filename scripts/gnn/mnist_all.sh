9400e6e84951625955620ed5dd673672477ca954
edebrouwer <edward.debrouwer@gmail.com>
2023-07-06 01:01:41 -0400

    script mnist gnn all

--- scripts/gnn/mnist_all.sh
+++ scripts/gnn/mnist_all.sh
@@ -3,7 +3,9 @@
 #SBATCH --job-name=gnn_mnist_dense
 #SBATCH --time=20:00:00
 #SBATCH --cpus-per-task=12
-#SBATCH --mem=64G
+#SBATCH --mem=32G
+#SBATCH --gpus=1
+#SBATCH --partition=gpu
 #SBATCH --output=./logs/slurm/%x_%j.out
 #SBATCH --error=./logs/slurm/%x_%j.err
 
