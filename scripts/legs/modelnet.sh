18b70929acb479678eaafa3d61092966a2941090
edebrouwer <edward.debrouwer@gmail.com>
2023-08-12 18:52:19 -0400

    legs scripts

---
+++ scripts/legs/modelnet.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --job-name=scattering_modelnet
+#SBATCH --time=20:00:00
+#SBATCH --cpus-per-task=12
+#SBATCH --mem=64G
+#SBATCH --output=./logs/slurm/%x_%j.out
+#SBATCH --error=./logs/slurm/%x_%j.err
+
+cd ~/project/pcnn
+module load miniconda
+conda activate pcnn
+
+python train.py -m experiment=legs/modelnet launcher=mccleary
